ce098a7650df5dababbe87fcf3208c128f132e56
Koolgee0 <50880695+Koolgee0@users.noreply.github.com>
2020-10-04 22:51:34 -0400

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Fibonacci {
+    public int getFibonacciValue(int n){
+        if (n <= 1)
+            return n;
+        return getFibonacciValue(n-1) + getFibonacciValue(n-2);
+
+
+
+    }
+}
